afe3b5b87cb59c1398da6b44b7fe8daadb2c3be1
Agyanshu kumar <agyanshukumar@gmail.com>
2024-10-15 10:06:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let box=document.querySelector(".colorbox");
 let btn=document.querySelector("button");
 
 function randomcol(){
-    let r=Math.floor(Math.random()*225);
-    let g=Math.floor(Math.random()*225);
-    let b=Math.floor(Math.random()*225);
+    let r=Math.floor(Math.random()*225+1);
+    let g=Math.floor(Math.random()*225+1);
+    let b=Math.floor(Math.random()*225+1);
     let color=`rgb(${r},${g},${b})`;
     box.style.backgroundColor=color;
     
